3d0dd9622ac58e28804c6559d2f4572bb8f8b1e8
Herbert Milhomme <herb@herbertmilhomme.com>
2019-10-22 11:19:45 -0400

    Update Moves.cs

--- PokemonUnity.Shared/Enums/Moves.cs
+++ PokemonUnity.Shared/Enums/Moves.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PokemonUnity;
+using PokemonUnity.Inventory;
 
 /// <summary>
 /// Namespace to nest all Pokemon Move Enums
